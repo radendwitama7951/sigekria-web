3bf33813b977dc591d32cd47cc24b6612428fbb3
dwitamabaliano045 <dwitamabaliano045@gmail.com>
2024-12-20 00:15:03 +0800

    remove environments files

--- src/environments/environment.development.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  SIGEKRIA_API: 'http://localhost:8000/api/v0',
-};
--- src/environments/environment.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  SIGEKRIA_API: 'https://main-bvxea6i-r6ymxwool6opu.us.platformsh.site/api/v0',
-};
